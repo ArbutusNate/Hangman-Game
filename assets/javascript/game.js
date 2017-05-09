6b4380289ac6057fb2cb39db7819171cdb0b148e
ArbutusNate <arbutusnate@gmail.com>
2017-05-09 12:03:35 -0700

    Styling and basic assignment done - just the bonus now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,57 @@
 // Set Variables
-	//Comp Chooses a Word
 var wordbank = ["tomb", "temple", "treasure", "explore", "rope", "pitfall", "snakes", "scarab", "boulder", "escape", "trap", "darts", "mummy", "ruins", "loot", 
 				"pilfer", "ancient", "lost", "skeleton", "detonate", "dig", "delve", "dark", "stone", "rock", "whip", "lasso"];
-
-var length = word.length
-  console.log(word);
-  // console.log(length);
 var wins = 0;
+var word = "";
+var playerChoice;
 var guessleft = 10;
-  // console.log(guessleft);
 var guesshistory = [];
 var wordblank = [];
-
-
-//Page Setup
-var compChoice = wordbank[Math.floor(Math.random() * wordbank.length)];
-var word = compChoice.split([]);
-for (i = 0; i < length; i++) { 
-    wordblank.push("_");
-}
-document.getElementById("pwordblank").innerHTML = wordblank;
-
-
-//On Keypress
-document.onkeyup = function(play){
-	var playerChoice = event.key;
-
-	//Wrong Guess
-	if(word.indexOf(playerChoice) === -1 && guesshistory.indexOf(playerChoice) === -1){
-		guesshistory.push(playerChoice);
-		guessleft--;
-		document.getElementById("guessleft").innerHTML = guessleft;
-		document.getElementById("guesshistory").innerHTML = guesshistory;
-		console.log("New Wrong Guess. playerChoice: " + playerChoice + ". guesshistory: " + guesshistory + ".")
+var wincondition = true;
+document.onkeyup = function(event){  //  --- On Keypress ---
+	playerChoice = event.key;
+	if(wincondition === true || guessleft === 0){ //Reset
+		compChoice = wordbank[Math.floor(Math.random() * wordbank.length)]; // Choose random word.
+		word = compChoice.split([]); // Split word into [comChoice].
+		length = word.length; // Set variable for blanking for loop.
+		wordblank = [];
+		for (i = 0; i < length; i++) {  // for loop
+			wordblank.push("_"); // Add _ equal to index of word
+		}
+		wincondition = false;
+		guessleft = 10;
+		guesshistory = [];
+		console.log(word);
+		document.getElementById("guessleft").innerHTML = guessleft; //Update guessleft in HTML.
+		document.getElementById("pwordblank").innerHTML = wordblank.join(" ");
+		document.getElementById("messages").innerHTML = "Press a letter to guess!";
+		document.getElementById("guesshistory").innerHTML = "Nothing yet!";
 	}
-	//Right Guess
-	else if(word.indexOf(playerChoice) > -1 && guesshistory.indexOf(playerChoice) === -1){
-		for (var i = 0; i < word.length; i++) {
-			//Add letter(s) to wordblank.
-			if(word[i] === playerChoice){
-			wordblank[i] = playerChoice;
-			document.getElementById("pwordblank").innerHTML = wordblank;
-			console.log("your guess " + playerChoice + " is correct, index " + i);
-				//Check for and display wins
-				if(wordblank.indexOf("_") === -1){
-					wins++;
-					document.getElementById("wins").innerHTML = wins;
-					console.log("You Win!")
+	else {	// Play
+		if(word.indexOf(playerChoice) === -1 && guesshistory.indexOf(playerChoice) === -1){ //Wrong Guess. playerChoice doesn't exist in [word] or [guesshistory]
+			guesshistory.push(playerChoice); //Add playerchoice to guesshistory.
+			guessleft--; //Reduce guessleft by 1.
+			document.getElementById("guessleft").innerHTML = guessleft; //Update guessleft in HTML.
+			document.getElementById("guesshistory").innerHTML = guesshistory; //Update guesshistory in HTML.
+			// console.log("New Wrong Guess. playerChoice: " + playerChoice + ". guesshistory: " + guesshistory + ".");
+		}
+		else if(word.indexOf(playerChoice) > -1 && guesshistory.indexOf(playerChoice) === -1){ // Right guess. playerChoice exists in [word].
+			for (var i = 0; i < word.length; i++) { // Loop = to word length.
+				if(word[i] === playerChoice){ // Add letter(s) to wordblank.
+				wordblank[i] = playerChoice;
+				document.getElementById("pwordblank").innerHTML = wordblank.join(" ");
+				// console.log("your guess " + playerChoice + " is correct, index " + i);
+					if(wordblank.indexOf("_") === -1){  //Check for and display wins
+						wins++;
+						wincondition = true;
+						document.getElementById("wins").innerHTML = wins; //Add HTML and update w/ "press any key to continue"
+						document.getElementById("messages").innerHTML = "You win! Press a letter to play again!";
+						console.log("You Win!");
+					}
 				}
 			}
-			
-      	}
+		}
 	}
-}
+};
 
 
